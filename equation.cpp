229e2f1fffe9f64ab2e7671cf616beaa94cd9b93
mregorova <69008784+mregorova@users.noreply.github.com>
2021-08-20 22:47:41 +0300

    Add files via upload
    Little bit more beauty

--- equation.cpp
+++ equation.cpp
@@ -4,15 +4,16 @@
 #include <math.h>
 
 
-int main(){
-    float a = 0, b = 0, c = 0;
-
+int main() {
+    float a = 0;
     printf("Enter a: "); //entering coefficients
     scanf("%f", &a);
 
+    float b = 0;
     printf("Enter b: ");
     scanf("%f", &b);
 
+    float c = 0;
     printf("Enter c: ");
     scanf("%f", &c);
 
@@ -21,32 +22,25 @@ int main(){
         x = -c / b;
 
         printf("x = %f", x);
-    }
-    else {
+    } else {
         float d = 0;
         d = pow(b, 2) - 4 * a * c; //counting discriminant
-
-        float x1 = 0, x2 = 0;
-
         if (d >= 0) { //discovering different types of solutions
             if (d == 0) {
                 x = (-b) / (a * 2);
 
                 printf("x = %f", x);
-            }
-            else {
+            } else {
+                float x1 = 0;
                 x1 = (-b + sqrt(d)) / (a * 2);
+                float x2 = 0;                
                 x2 = (-b - sqrt(d)) / (a * 2);
 
                 printf("x1 = %f x2 = %f", x1, x2);
             }
-        }
-        else {
+        } else {
             printf("No solutions :(");
         }
     }
-
-    
-
     return 0;
 }

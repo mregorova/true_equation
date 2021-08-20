4aad52d16c7bda0cc921a6eaf4630dda02472479
mregorova <69008784+mregorova@users.noreply.github.com>
2021-08-20 17:37:16 +0300

    Add files via upload
    TODO1 from 19.08.21 is done

--- equation.cpp
+++ equation.cpp
@@ -1,16 +1,13 @@
-﻿
-
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS //turning off the warnings to make this thing work
 
 #include <stdio.h>
 #include <math.h>
 
 
-int main()
-{
-    float a,b,c;
+int main(){
+    float a = 0, b = 0, c = 0;
 
-    printf("Enter a: ");
+    printf("Enter a: "); //entering coefficients
     scanf("%f", &a);
 
     printf("Enter b: ");
@@ -19,14 +16,37 @@ int main()
     printf("Enter c: ");
     scanf("%f", &c);
 
-    float d;
-    d = pow(b,2) - 4*a*c;
+    float x = 0;
+    if (a == 0) { //checking if our equation is quadratic
+        x = -c / b;
 
-    float x1, x2;
-    x1 = (-b + sqrt(d)) / (a*2);
-    x2 = (-b - sqrt(d)) / (a*2);
+        printf("x = %f", x);
+    }
+    else {
+        float d = 0;
+        d = pow(b, 2) - 4 * a * c; //counting discriminant
 
-    printf("x1 = %f x2 = %f", x1,x2);
+        float x1 = 0, x2 = 0;
+
+        if (d >= 0) { //discovering different types of solutions
+            if (d == 0) {
+                x = (-b) / (a * 2);
+
+                printf("x = %f", x);
+            }
+            else {
+                x1 = (-b + sqrt(d)) / (a * 2);
+                x2 = (-b - sqrt(d)) / (a * 2);
+
+                printf("x1 = %f x2 = %f", x1, x2);
+            }
+        }
+        else {
+            printf("No solutions :(");
+        }
+    }
+
+    
 
     return 0;
 }

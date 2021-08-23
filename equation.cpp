a1cb838340c2725f7cea6e9e713688385903a7cf
mregorova <69008784+mregorova@users.noreply.github.com>
2021-08-23 21:37:22 +0300

    Add files via upload
    Now everything is in functions!

--- equation.cpp
+++ equation.cpp
@@ -18,30 +18,53 @@ int main() {
     scanf("%f", &c);
 
     if (a == 0) { //checking if our equation is quadratic
-        float x4 = 0;
-        x4 = -c / b;
-
-        printf("x = %f", x4);
-    } else {
-        float d = 0;
-        d = pow(b, 2) - 4 * a * c; //counting discriminant
-        if (d >= 0) { //discovering different types of solutions
-            if (d == 0) {
-                float x3 = 0;
-                x3 = (-b) / (a * 2);
-
-                printf("x = %f", x3);
-            } else {
-                float x1 = 0;
-                x1 = (-b + sqrt(d)) / (a * 2);
-                float x2 = 0;                
-                x2 = (-b - sqrt(d)) / (a * 2);
-
-                printf("x1 = %f x2 = %f", x1, x2);
-            }
-        } else {
-            printf("No solutions :(");
-        }
+        void lineal(float a, float b, float c);
+        lineal(a, b, c);
+    }
+    else {
+        void quadratic(float a, float b, float c);
+        quadratic(a, b, c);
     }
     return 0;
+}
+
+void lineal(float a, float b, float c)
+{
+    float x4 = 0;
+    x4 = -c / b;
+    printf("x = %f", x4);
+}
+
+void quadratic(float a, float b, float c)
+{
+    float d = 0;
+    float discriminant(float a, float b, float c);
+    d = discriminant(a, b, c); //counting discriminant
+
+    if (d >= 0) { //discovering different types of solutions
+        if (d == 0) {
+            float x3 = 0;
+            x3 = (-b) / (a * 2);
+
+            printf("x = %f", x3);
+        }
+        else {
+            float x1 = 0;
+            x1 = (-b + sqrt(d)) / (a * 2);
+            float x2 = 0;
+            x2 = (-b - sqrt(d)) / (a * 2);
+
+            printf("x1 = %f x2 = %f", x1, x2);
+        }
+    }
+    else {
+        printf("No solutions :(");
+    }
+}
+
+float discriminant(float a, float b, float c)
+{
+    float d = 0;
+    d = pow(b, 2) - 4 * a * c; 
+    return d;
 }

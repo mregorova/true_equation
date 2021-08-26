a12e89d936acd9a731f545265db98dc329b7a06a
mregorova <69008784+mregorova@users.noreply.github.com>
2021-08-26 16:45:36 +0300

    Add files via upload
    Have fun with short function "solve" for all the equation situations and other pretty stuff

--- equation.cpp
+++ equation.cpp
@@ -3,93 +3,156 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 /*
-1) ??????? ???? ?????? ?? 3 ?????????????
-2) ?????????, ??? ??????????? ??? ?????? 3 ??????????
-3) ???????????? ?????? ? ???????????: ????????? ???????????? ? ??????? ?? ??????????(&), ?????????????? ???????? ??????????
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 4, c = 0;
+double a = 20, b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
+double a = 0,  b = 0, c = 0;
 */
 
-/// TODO:
-///4) - assert(isfinite(...))
-
+// Keep reading from input stream until a newline is read
 void flushInput()
 {
-    // Keep reading from input stream until a newline is read
     int c;
+
     do
     {
-        if ((c = getchar()) == EOF) exit(1);
+        if ((c = getchar()) == EOF)
+        {
+            exit(1);
+        }
     } while (c != '\n');
 }
 
-void linear(double a, double b, double c)
+int linear(double a, double b, double c, double* x1)
 {
-    if (b == 0) {
-        if (c == 0) {
-            printf("Infinity number of solutions\n");
+    if (b == 0)
+    {
+        if (c == 0)
+        {
+            return -1;
         }
-        else {
-            printf("No solutions :(\n");
-        }
-    }
-    else {
-        double x4 = 0;
-        x4 = -c / b;
-        printf("x = %f", x4);
-    }
-    //return 0;
-}
 
-void quadratic(double a, double b, double c)
-{
-    double d = 0;
-    double discriminant(double a, double b, double c);
-    d = discriminant(a, b, c); // counting discriminant
-
-    if (d >= 0) { // discovering different types of solutions
-        if (d == 0) {
-            double x3 = 0;
-            x3 = (-b) / (a * 2);
-
-            printf("x = %f", x3);
-        }
-        else {
-            double x1 = 0;
-            x1 = (-b + sqrt(d)) / (a * 2);
-            double x2 = 0;
-            x2 = (-b - sqrt(d)) / (a * 2);
-
-            printf("x1 = %f x2 = %f", x1, x2);
+        else
+        {
+            return 0;
         }
     }
-    else {
-        printf("No solutions :(");
+
+    else
+    {
+        *x1 = -c / b;
+        return 1;
     }
+
+    return 777;
 }
 
 double discriminant(double a, double b, double c)
 {
-    double d = 0;
-    d = pow(b, 2) - 4 * a * c;
+    double d = pow(b, 2) - 4 * a * c;
+
     return d;
 }
 
+int quadratic(double a, double b, double c, double* x1, double* x2)
+{
+    double d = 0;
+    d = discriminant(a, b, c); // counting discriminant
+
+    if (d >= 0)
+    { // discovering different types of solutions
+        if (d == 0)
+        {
+            *x1 = (-b) / (a * 2);
+
+            return 1;
+        }
+
+        else
+        {
+            *x1 = (-b + sqrt(d)) / (a * 2);
+            *x2 = (-b - sqrt(d)) / (a * 2);
+
+            return 2;
+        }
+    }
+
+    return 0;
+}
+
+
+int solve(double a, double b, double c, double* x1, double* x2)
+{
+    if (a == 0)
+    {
+        return linear(a, b, c, x1);
+    }
+    else
+    {
+        return quadratic(a, b, c, x1, x2);
+    }
+}
+
 int main() {
-    double a = 0, b = 0, c = 0;
+
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    double x2 = 0;
+    double x1 = 0;
 
     printf("Enter coefficients a b c: \n");
-    while (scanf("%lg %lg %lg", &a, &b, &c) != 3) {
+
+    while (scanf("%lg %lg %lg", &a, &b, &c) != 3)
+    {
         printf("Please try again\n");
+
         scanf("%lg %lg %lg", &a, &b, &c);
+
+        // fflush(stdin);
+
         flushInput();
+
     }
 
-    if (a == 0) { // checking if our equation is quadratic
-        linear(a, b, c);
+    solve(a, b, c, &x1, &x2);
+
+    //assert(x1 != NULL);
+    //assert(x2 != NULL);
+
+    // assert(std::isfinite(a));
+    // assert(isfinite(b));
+    // assert(isfinite(c));
+
+    switch (solve(a, b, c, &x1, &x2))
+    {
+    case -1:
+        printf("Infinite number of solutions");
+        break;
+    case 0:
+        printf("No solutions :(");
+        break;
+    case 1:
+        printf("x = %lg", x1);
+        break;
+    case 2:
+        printf("x1 = %lg x2 = %lg", x1, x2);
+        break;
+    default:
+        printf("Something went wrong");
+        break;
     }
-    else {
-        quadratic(a, b, c);
-    }
+
+
     return 0;
 }
